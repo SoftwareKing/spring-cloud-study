926541138baf63563a16923a805692ae26c20202
xujin <Software_King@qq.com>
2017-02-07 23:03:03 +0800

    init

---
+++ sc-eureka-first/sc-eureka-first-order/src/main/java/org/xujin/sc/eureka/first/order/App.java
@@ -0,0 +1,13 @@
+package org.xujin.sc.eureka.first.order;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
