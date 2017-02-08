9375232ccba56202c038de646286ae07d781c76a
xujin <Software_King@qq.com>
2017-02-08 23:25:23 +0800

    Eureka server

---
+++ sc-eureka-first/sc-eureka-first-server/src/main/java/org/xujin/sc/eureka/server/SpringCloudEurekaServer.java
@@ -0,0 +1,15 @@
+package org.xujin.sc.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaServer {
+	
+  public static void main(String[] args) {
+	SpringApplication.run(SpringCloudEurekaServer.class,args);
+	
+  }
+}
