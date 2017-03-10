2d0dd5c6c94dad9a49cb3d4561fab2e376a5ee15
xujin <Software_King@qq.com>
2017-03-10 18:04:33 +0800

    增加Spring Boot端点和注释

--- sc-eureka-first/sc-eureka-first-user/src/main/java/org/xujin/sc/eureka/user/controller/UserController.java
+++ sc-eureka-first/sc-eureka-first-user/src/main/java/org/xujin/sc/eureka/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.xujin.sc.eureka.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,15 @@ public class UserController {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	// 从属性文件中读取服务提供的URL
+	@Value("order.orderServiceUrl")
+	private String oderServiceUrl;
+
 	@GetMapping("/user/{id}")
 	public Order findById(@PathVariable Long id) {
-		return this.restTemplate.getForObject("http://localhost:8000/sc/order/" + id,
+		// this.restTemplate.getForObject("http://localhost:8000/sc/order/" +
+		// id,Order.class)在代码中写死需要调用的URL
+		return this.restTemplate.getForObject(oderServiceUrl + "sc/order/" + id,
 				Order.class);
 	}
 }
