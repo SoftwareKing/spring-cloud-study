309ac10cc2b05cf07bb022d90f3491b3af175515
xujin <Software_King@qq.com>
2017-03-17 00:00:55 +0800

    修改

--- sc-eureka-first/sc-eureka-first-consumer/src/main/java/org/xujin/sc/eureka/user/UserConsumerApplication.java
+++ sc-eureka-first/sc-eureka-first-consumer/src/main/java/org/xujin/sc/eureka/user/UserConsumerApplication.java
@@ -3,6 +3,7 @@ package org.xujin.sc.eureka.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 public class UserConsumerApplication {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- sc-eureka-first/sc-eureka-first-consumer/src/main/java/org/xujin/sc/eureka/user/controller/UserController.java
+++ sc-eureka-first/sc-eureka-first-consumer/src/main/java/org/xujin/sc/eureka/user/controller/UserController.java
@@ -2,10 +2,13 @@ package org.xujin.sc.eureka.user.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,7 @@ import org.xujin.sc.eureka.user.model.Order;
 
 @RestController
 public class UserController {
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private RestTemplate restTemplate;
@@ -21,9 +25,29 @@ public class UserController {
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+
 	// 从属性文件中读取服务提供的URL
 	@Value("order.orderServiceUrl")
 	private String oderServiceUrl;
+
+	@GetMapping("/log-instance")
+	public void logUserInstance() {
+		ServiceInstance serviceInstance = this.loadBalancerClient
+				.choose("sc-eureka-first-provider");
+		String url = String.valueOf(serviceInstance.getHost());
+
+		logger.info("{},{},{}", serviceInstance.getServiceId(), serviceInstance.getHost(),
+				serviceInstance.getPort());
+
+	}
+
+	@GetMapping("/ribbon/user/{id}")
+	public Order findByIdByRibbon(@PathVariable Long id) {
+		return this.restTemplate.getForObject(
+				"sc-eureka-first-provider" + "/sc/order/" + id, Order.class);
+	}
 
 	public String serviceUrl() {
 		List<ServiceInstance> list = discoveryClient
